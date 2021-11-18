6b6da115f1543e203f928cc610c416067d41a806
Mortron <cmsl4y3r@gmail.com>
2021-11-17 19:49:05 -0700

    Not much done, but it's a start

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = (props) => {
+    
+
+
+    return(
+        <div>
+            Search for: Button Goes HERE.  ID: input HERE 
+            <button>Search</button>
+        </div>
+
+    )
+}
+export default Home;
